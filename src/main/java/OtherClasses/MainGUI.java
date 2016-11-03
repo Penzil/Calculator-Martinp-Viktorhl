c10dc2ca42bdfdea16eeaaed0b60b3d6eb5dbe9d
Martin Palman <martin@ellva.se>
2016-11-03 16:49:19 +0100

    i finely have the final design of the calculater with buttons and all, but i cant press result or the other operation buttons

--- src/main/java/OtherClasses/MainGUI.java
+++ src/main/java/OtherClasses/MainGUI.java
@@ -11,6 +11,8 @@ import OtherClasses.*;
 
 import javax.swing.JButton;
 import static java.lang.Math.*;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 public class MainGUI {
 
 	
@@ -20,6 +22,13 @@ public class MainGUI {
 	private AdvancedCalculator advancedCalculator = new AdvancedCalculator();
 	private BasicCalculator basicCalculator = new BasicCalculator();
 	Scanner scanner = new Scanner(System.in);
+	
+	double firstNumber;
+	double secondNumber;
+	double result;
+	String operations;
+	String answer;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -49,14 +58,17 @@ public class MainGUI {
 	JButton btnSub = new JButton("-");
 	JButton btnMulty = new JButton("*");
 	JButton btnDiv = new JButton("/");
-	JButton btnDott = new JButton(".");
+	JButton btnDot = new JButton(".");
 	JButton btnAbs = new JButton("ABS");
 	JButton btnPi = new JButton("PI");
 	JButton btnMin = new JButton("MIN");
 	JButton btnMax = new JButton("MAX");
 	JButton btnRound = new JButton("ROUND");
 	JButton btnFloor = new JButton("FLOOR");
-	
+	JButton btnResult = new JButton("=");
+	JButton btnPlusMinus = new JButton("+/-");
+	JButton btnProcent = new JButton("%");
+	JButton btnC = new JButton("C");
 	
 	ActionListener actionListener = new ActionListener() {
 
@@ -64,6 +76,7 @@ public class MainGUI {
 			buttonPressed(e);
 		}
 	};
+	
 
 
 
@@ -79,35 +92,72 @@ public class MainGUI {
 		Calculator.getContentPane().setBackground(new Color(72, 209, 204));
 
 		Calculator.setTitle("Calculator");
-		Calculator.setBounds(100, 100, 485, 300);
+		Calculator.setBounds(100, 100, 640, 495);
 		Calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Calculator.getContentPane().setLayout(null);
+		number0.setFont(new Font("Tahoma", Font.BOLD, 25));
 		
-		number0.setBounds(239, 205, 90, 25);
-		number1.setBounds(239, 167, 39, 25);
-		number2.setBounds(290, 167, 39, 25);
-		number3.setBounds(341, 167, 39, 25);
-		number4.setBounds(239, 129, 39, 25);
-		number5.setBounds(290, 129, 39, 25);
-		number5.setBounds(290, 129, 39, 25);
-		number6.setBounds(341, 129, 39, 25);
-		number7.setBounds(239, 89, 39, 25);
-		number8.setBounds(290, 89, 39, 25);
-		number9.setBounds(341, 91, 39, 25);
-		btnAdd.setBounds(392, 91, 50, 25);
-		btnSub.setBounds(392, 129, 50, 25);
-		btnMulty.setBounds(392, 167, 50, 25);
-		btnDiv.setBounds(392, 205, 50, 25);
-		btnDott.setBounds(341, 205, 39, 25);
-		btnAbs.setBounds(12, 89, 97, 25);
-		btnPi.setBounds(121, 89, 97, 25);
-		btnMin.setBounds(12, 129, 97, 25);
-		btnMax.setBounds(121, 129, 97, 25);
-		btnRound.setBounds(12, 167, 97, 25);
-		btnFloor.setBounds(121, 167, 97, 25);
+		number0.setBounds(12, 362, 150, 75);
+		btnDot.setBounds(162, 362, 75, 75);
+		btnResult.setBounds(237,362,75,75);
+		
+		number1.setBounds(12, 287, 75, 75);
+		number2.setBounds(87, 287, 75, 75);
+		number3.setBounds(162, 287, 75, 75);
+		btnDiv.setBounds(237, 287, 75, 75);
+		
+		number4.setBounds(12, 212, 75, 75);
+		number5.setBounds(87, 212, 75, 75);
+		number6.setBounds(162, 212, 75, 75);
+		btnMulty.setBounds(237, 212, 75, 75);
+		
+		number7.setBounds(12, 137, 75, 75);
+		number8.setBounds(87, 137, 75, 75);
+		number9.setBounds(162, 137, 75, 75);
+		btnSub.setBounds(237, 137, 75, 75);
+		
+		btnC.setBounds(12, 62, 75, 75);
+		btnProcent.setBounds(87, 62, 75, 75);
+		btnPlusMinus.setBounds(162, 62, 75, 75);
+		btnAdd.setBounds(237, 62, 75, 75);
+		
+		btnAbs.setBounds(315, 62, 130, 75);
+		btnPi.setBounds(480, 62, 130, 75);
+		btnMin.setBounds(315, 175, 130, 75);
+		btnMax.setBounds(480, 175, 130, 75);
+		btnRound.setBounds(315, 287, 130, 75);
+		btnFloor.setBounds(480, 287, 130, 75);
+		
+		
+		number0.setFont(new Font("Tahoma", Font.BOLD, 18));
+		number1.setFont(new Font("Tahoma", Font.BOLD, 25));
+		number2.setFont(new Font("Tahoma", Font.BOLD, 25));
+		number3.setFont(new Font("Tahoma", Font.BOLD, 25));
+		number4.setFont(new Font("Tahoma", Font.BOLD, 25));
+		number5.setFont(new Font("Tahoma", Font.BOLD, 25));
+		number6.setFont(new Font("Tahoma", Font.BOLD, 25));
+		number7.setFont(new Font("Tahoma", Font.BOLD, 25));
+		number8.setFont(new Font("Tahoma", Font.BOLD, 25));
+		number9.setFont(new Font("Tahoma", Font.BOLD, 25));
+		btnResult.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnDot.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnAdd.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnSub.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnMulty.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnDiv.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnAbs.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnPi.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnMin.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnMax.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnRound.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnFloor.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnPlusMinus.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnProcent.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnC.setFont(new Font("Tahoma", Font.BOLD, 18));
 		
 		textField = new JTextField();
-		textField.setBounds(12, 33, 443, 43);
+		textField.setHorizontalAlignment(SwingConstants.LEFT);
+		textField.setBounds(12, 13, 300, 43);
 		Calculator.getContentPane().add(textField);
 		textField.setColumns(10);
 	}
@@ -126,14 +176,17 @@ public class MainGUI {
 			Calculator.getContentPane().add(btnSub);
 			Calculator.getContentPane().add(btnMulty);
 			Calculator.getContentPane().add(btnDiv);
-			Calculator.getContentPane().add(btnDott);
+			Calculator.getContentPane().add(btnDot);
 			Calculator.getContentPane().add(btnAbs);
 			Calculator.getContentPane().add(btnPi);
 			Calculator.getContentPane().add(btnMin);
 			Calculator.getContentPane().add(btnMax);
 			Calculator.getContentPane().add(btnRound);
 			Calculator.getContentPane().add(btnFloor);
-			
+			Calculator.getContentPane().add(btnResult);
+			Calculator.getContentPane().add(btnPlusMinus);
+			Calculator.getContentPane().add(btnProcent);
+			Calculator.getContentPane().add(btnC);
 		}
 			public void addActionListerners() {
 				number0.addActionListener(actionListener);
@@ -150,8 +203,129 @@ public class MainGUI {
 				btnSub.addActionListener(actionListener);
 				btnMulty.addActionListener(actionListener);
 				btnDiv.addActionListener(actionListener);
-				btnDott.addActionListener(actionListener);
+				btnDot.addActionListener(actionListener);
+				btnResult.addActionListener(actionListener);
 }
-		
+			public void buttonPressed(ActionEvent arg0) {
+				if (arg0.getSource() == number0){
+					String EnterNumber = textField.getText()+ number0.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == number1){
+					String EnterNumber = textField.getText()+ number1.getText();
+					textField.setText(EnterNumber);
+				}		
+				if (arg0.getSource() == number2) {
+					String EnterNumber = textField.getText()+ number2.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == number3) {
+					String EnterNumber = textField.getText()+ number3.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == number4) {
+					String EnterNumber = textField.getText()+ number4.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == number5) {
+					String EnterNumber = textField.getText()+ number5.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == number6) {
+					String EnterNumber = textField.getText()+ number6.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == number7) {
+					String EnterNumber = textField.getText()+ number7.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == number8) {
+					String EnterNumber = textField.getText()+ number8.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == number9) {
+					String EnterNumber = textField.getText()+ number9.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == btnDot) {
+					String EnterNumber = textField.getText()+ btnDot.getText();
+					textField.setText(EnterNumber);
+				}
+				if (arg0.getSource() == btnC){
+					
+					textField.setText(null);
+					
+				}
+				if (arg0.getSource() == btnAdd) {
+					firstNumber = Double.parseDouble(textField.getText());
+					textField.setText("");
+					operations = "+";
+				}	
+				if (arg0.getSource() == btnSub) {
+					firstNumber = Double.parseDouble(textField.getText());
+					textField.setText("");
+					operations = "-";
+				}	
+				if (arg0.getSource() == btnMulty) {
+					firstNumber = Double.parseDouble(textField.getText());
+					textField.setText("");
+					operations = "*";
+				}	
+				if (arg0.getSource() == btnDiv) {
+					firstNumber = Double.parseDouble(textField.getText());
+					textField.setText("");
+					operations = "/";
+				}	
+				if (arg0.getSource() == btnProcent) {
+					firstNumber = Double.parseDouble(textField.getText());
+					textField.setText("");
+					operations = "%";
+				}	
+				if (arg0.getSource() == btnPlusMinus) {
+					double ops = Double.parseDouble(textField.getText());
+					ops = ops*(-1);
+					textField.setText(String.valueOf(ops));
+					
+				}	
+				
+				if (arg0.getSource() == btnResult) {
+					
+					String answer;
+					secondNumber= Double.parseDouble(textField.getText());
+					
+					if (operations == "+"){
+						result = firstNumber+secondNumber;
+						answer = String.format("%.2f, result)");
+						textField.setText(answer);		
+				}
+				
+					else if(operations == "-"){
+						result = firstNumber-secondNumber;
+						answer = String.format("%.2f, result)");
+						textField.setText(answer);
+				}	
+				
+					else if(operations == "*"){
+						result = firstNumber*secondNumber;
+						answer = String.format("%.2f, result)");
+						textField.setText(answer);	
+				}
+				
+					if(operations == "/"){
+						result = firstNumber/secondNumber;
+						answer = String.format("%.2f, result)");
+						textField.setText(answer);	
+				}	
+					if (operations == "%"){
+						result = firstNumber%secondNumber;
+						answer = String.format("%.2f, result)");
+						textField.setText(answer);	
+				}		
+	
+				
+			
+			
 
+}
+}
 }
